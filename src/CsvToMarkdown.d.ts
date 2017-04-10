57a8b62e0fa416b4757be81e74328895d13eea66
Jesse Donat <jdonat@myon.com>
2017-04-10 17:21:31 -0500

    Rewrites in TypeScript

---
+++ src/CsvToMarkdown.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Converts CSV to Markdown Table
+ *
+ * @param {string} csvContent - The string content of the CSV
+ * @param {string} delimiter - The character(s) to use as the CSV column delimiter
+ * @param {boolean} hasHeader - Whether to use the first row of Data as headers
+ * @returns {string}
+ */
+declare function csvToMarkdown(csvContent: string, delimiter: string, hasHeader: boolean): string;
---
+++ src/CsvToMarkdown.ts
@@ -0,0 +1,83 @@
+/**
+ * Converts CSV to Markdown Table
+ *
+ * @param {string} csvContent - The string content of the CSV
+ * @param {string} delimiter - The character(s) to use as the CSV column delimiter
+ * @param {boolean} hasHeader - Whether to use the first row of Data as headers
+ * @returns {string}
+ */
+function csvToMarkdown(csvContent: string, delimiter: string, hasHeader: boolean) {
+	if (delimiter != "\t") {
+		csvContent = csvContent.replace(/\t/g, "    ");
+	}
+
+	let columns = csvContent.split("\n");
+
+	let tabularData: string[][] = [];
+	let maxRowLen: number[] = [];
+
+	columns.forEach((e, i) => {
+		if (typeof tabularData[i] == "undefined") {
+			tabularData[i] = [];
+		}
+
+		let row = e.split(delimiter);
+
+		row.forEach((ee, ii) => {
+			if (typeof maxRowLen[ii] == "undefined") {
+				maxRowLen[ii] = 0;
+			}
+
+			maxRowLen[ii] = Math.max(maxRowLen[ii], ee.length);
+			tabularData[i][ii] = ee;
+		});
+	});
+
+	let headerOutput = "";
+	let seperatorOutput = "";
+
+	maxRowLen.forEach((len) => {
+		let spacer;
+		spacer = Array(len + 1 + 2).join("-");
+		seperatorOutput += "|" + spacer;
+
+		spacer = Array(len + 1 + 2).join(" ");
+		headerOutput += "|" + spacer;
+	});
+
+	headerOutput += "| \n";
+	seperatorOutput += "| \n";
+
+	if (hasHeader) {
+		headerOutput = "";
+	}
+
+	let rowOutput = "";
+	let initHeader = true;
+	tabularData.forEach((col) => {
+		maxRowLen.forEach((len, y) => {
+			let row = typeof col[y] == "undefined" ? "" : col[y];
+			let spacing = Array((len - row.length) + 1).join(" ");
+			let out = `| ${row}${spacing} `;
+			if (hasHeader && initHeader) {
+				headerOutput += out;
+			} else {
+				rowOutput += out;
+			}
+		});
+
+		if (hasHeader && initHeader) {
+			headerOutput += "| \n";
+		} else {
+			rowOutput += "| \n";
+		}
+
+		initHeader = false;
+	});
+
+	return headerOutput + seperatorOutput + rowOutput;
+}
+
+if (typeof module != "undefined") {
+	module.exports = csvToMarkdown;
+}
