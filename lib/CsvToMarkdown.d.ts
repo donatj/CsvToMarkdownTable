f086b1702f2683e10c5138f071b7a435174fc2c8
Terri Ann <terri.ann.s@gmail.com>
2017-06-14 22:43:37 -0400

    Adding functionality for double bar header

--- lib/CsvToMarkdown.d.ts
+++ lib/CsvToMarkdown.d.ts
@@ -4,6 +4,7 @@
  * @param {string} csvContent - The string content of the CSV
  * @param {string} delimiter - The character(s) to use as the CSV column delimiter
  * @param {boolean} hasHeader - Whether to use the first row of Data as headers
+ * @param {boolean} useDoubleBarHeader - Whether the header uses double barlines for <th>
  * @returns {string}
  */
-declare function csvToMarkdown(csvContent: string, delimiter?: string, hasHeader?: boolean): string;
+declare function csvToMarkdown(csvContent: string, delimiter?: string, hasHeader?: boolean, useDoubleBarHeader?: boolean): string;
--- src/CsvToMarkdown.ts
+++ src/CsvToMarkdown.ts
@@ -4,9 +4,10 @@
  * @param {string} csvContent - The string content of the CSV
  * @param {string} delimiter - The character(s) to use as the CSV column delimiter
  * @param {boolean} hasHeader - Whether to use the first row of Data as headers
+ * @param {boolean} useDoubleBarHeader - Whether the header uses double barlines for <th>
  * @returns {string}
  */
-function csvToMarkdown(csvContent: string, delimiter: string = "\t", hasHeader: boolean = false) {
+function csvToMarkdown(csvContent: string, delimiter: string = "\t", hasHeader: boolean = false, useDoubleBarHeader: boolean = false) {
 	if (delimiter != "\t") {
 		csvContent = csvContent.replace(/\t/g, "    ");
 	}
@@ -53,8 +54,31 @@ function csvToMarkdown(csvContent: string, delimiter: string = "\t", hasHeader: 
 	tabularData.forEach((col, i) => {
 		maxRowLen.forEach((len, y) => {
 			const row = typeof col[y] == "undefined" ? "" : col[y];
-			const spacing = Array((len - row.length) + 1).join(" ");
-			const out = `| ${row}${spacing} `;
+			const headerWidth = useDoubleBarHeader ? 2 : 1;
+
+			let pre_space_count = (i > 0) ? headerWidth : 1;
+			let post_space_count = (len - row.length ) + 1;
+
+			if(useDoubleBarHeader) {
+				if(i > 0 ){
+					post_space_count++;
+					if(y > 0){
+						pre_space_count--;
+					}
+				}
+				if(!hasHeader && i===0){
+					post_space_count++;
+					if(y === 0) {
+						pre_space_count++;
+					}
+				}
+			}
+			
+			const preSpacing = Array(pre_space_count + 1).join(" ");
+			const spacing = Array(post_space_count + 1).join(" ");
+
+			const out = `|${preSpacing}${row}${spacing}`;
+			
 			if (hasHeader && i === 0) {
 				headerOutput += out;
 			} else {
@@ -68,6 +92,11 @@ function csvToMarkdown(csvContent: string, delimiter: string = "\t", hasHeader: 
 			rowOutput += "| \n";
 		}
 	});
+
+	if(useDoubleBarHeader) {
+		headerOutput = headerOutput.replace(/\|/g, '||');
+		seperatorOutput = '';
+	}
 
 	return headerOutput + seperatorOutput + rowOutput;
 }
